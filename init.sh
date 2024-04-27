e02560ce9a221d0f8000e1712af35e8e5a666770
Guilherme Tiscoski <geetiscoski@Guilhermes-MacBook-Pro.local>
2024-04-27 20:07:54 -0300

    feat: remove ansible ppa because it is not needed

--- init.sh
+++ init.sh
@@ -6,9 +6,6 @@ sudo apt-get update
 # Install software-properties-common if not already installed (needed for add-apt-repository)
 sudo apt-get install -y software-properties-common
 
-# Add the official Ansible PPA (Personal Package Archive)
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-
 # Install Ansible
 sudo apt-get install -y ansible git
 
